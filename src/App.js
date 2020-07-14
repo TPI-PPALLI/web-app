8b4a07cf6b807683e6f4a38f3f6eb50cdf464c72
Clover243 <amyshao15@gmail.com>
2020-07-13 20:34:40 -0700

    basic web app set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Timer from './components/timer.js'
+import SocialMediaButtons from "./components/socialMediaButtons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo"/>
+                <p className="App-slogan">Empower You to Feel Awesome</p>
+            </div>
+            <body className="App-body">
+                <Timer></Timer>
+                <a
+                  className="App-link"
+                  href="https://chloeting.com/"
+                >
+                  Try a Workout!
+                </a>
+            </body>
+            <div className="App-footer">
+                <SocialMediaButtons url="www.google.com" text="Check out what I just did!"/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/socialMediaButtons.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {EmailShareButton, FacebookShareButton, TwitterShareButton} from "react-share";
+import {FacebookIcon, TwitterIcon, EmailIcon} from "react-share";
+
+// takes in a url and message from App to share on the social media platform
+const SocialMediaButtons = (props) => (
+    <div className="SocialMediaButtons"
+         style={{
+             position: 'absolute',
+             right: 10,
+             margin: '1%'
+         }}>
+        <TwitterShareButton
+            url={props.url}
+            title={props.text}
+            style={{marginRight: 10}}>
+            <TwitterIcon
+                size={32}
+                left={10}
+                round/>
+        </TwitterShareButton>
+        <FacebookShareButton
+            url={props.url}
+            title={props.text}
+            style={{marginRight: 10}}>
+            <FacebookIcon
+                size={32}
+                round/>
+        </FacebookShareButton>
+        <EmailShareButton
+            url={props.url}
+            title={props.text}
+            style={{marginRight: 10}}>
+            <EmailIcon
+                size={32}
+                round/>
+        </EmailShareButton>
+    </div>
+)
+
+export default SocialMediaButtons
