1c38e1261deb9b419d7da579821b08e3b55704d3
Clover243 <amyshao15@gmail.com>
2020-07-13 20:39:16 -0700

    basic web app set up

---
+++ src/components/timer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Timer extends Component {
+    // should connect to the chrome extension timer somehow
+    render() {
+        return (
+            <div>
+                <h1 style={{color:'#3241e4', fontSize:40}}>Time for a break!</h1>
+                <div style={{color:'#F05E50', fontSize:160}}>
+                    <time>0:30:59</time>
+                </div>
+                <br />
+            </div>
+        );
+    }
+}
